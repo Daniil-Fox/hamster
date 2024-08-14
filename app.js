bbb7967501f3130e6261599c4a3b355ad8266652
Artyushenko Daniil <foxones80@gmail.com>
2024-08-14 20:14:11 +0600

    fixed some

--- app.js
+++ app.js
@@ -128,88 +128,89 @@ if ($circle) {
 }
 start();
 
-const marketItems = [...document.querySelectorAll(".market-item")];
+// const marketItems = [...document.querySelectorAll(".market-item")];
+const marketList = document.querySelector('.market__list')
+if (marketList) {
+  const marketItemsArr = JSON.parse(localStorage.getItem('market-items')) ?? [
+    {
+      name: 'Boost',
+      cost: 100,
+      lvl: 1,
+      isActive: false,
+      desc: 'x1.4 к прибыли',
+      id: Math.floor(Date.now() + Math.random() * 100)
+    },
+    {
+      name: 'Energy',
+      cost: 100,
+      lvl: 1,
+      isActive: false,
+      desc: 'x1.2 к лимиту',
+      id: Math.floor(Date.now() + Math.random() * 100)
+    },
+  ]
+  function boostFunc(item){
 
-if (marketItems.length > 0) {
-  const marketItemsArr = []
+    if( item.cost <= getScore()){
 
-  marketItems.forEach((item, idx) => {
-    const upLvl = item.querySelector(".market-item__cost");
+      setProfitClick(getProfitClick() * 1.2)
+      setScore(getScore() -  item.cost)
 
-    
+      item.cost *= 2
+      item.lvl += 1
 
-    const infoItem = {}
-    infoItem.name = item.querySelector('.market-item__name').textContent.toLowerCase().trim()
-    infoItem.cost = +item.querySelector('.market-item__cost span').textContent
-    infoItem.lvl = +item.querySelector('.market-item__lvl span').textContent
-    infoItem.isActive = infoItem.cost < getScore()
-    
-    setMarketItemsCosts(infoItem)
-
-    if(!infoItem.isActive){
-      addBoutghtClass(item)
-    } else {
-      removeBoutghtClass(item)
-      if(infoItem.name == "boost") {
-        upLvl.addEventListener('click', e => {
-          const currItemCost = JSON.parse(localStorage.getItem('market-items'))[idx].cost
-          if(currItemCost < getScore()){
-
-            setProfitClick(getProfitClick() * 1.2)
-            setScore(getScore() - currItemCost)
-
-            infoItem.lvl += 1
-            infoItem.cost = +item.querySelector('.market-item__cost span').textContent * 2
-  
-  
-            const newInfo = {
-              name: marketItems[idx].name,
-              cost: infoItem.cost,
-              isActive: marketItems[idx].isActive,
-              lvl: infoItem.lvl + 1,
-            }
-            replaceItem(newInfo, idx)
-  
-            item.querySelector('.market-item__lvl span').textContent = infoItem.lvl
-            item.querySelector('.market-item__cost span').textContent = item.querySelector('.market-item__cost span').textContent * 2
-          }
-          
-        })
-      }
-
-      if(infoItem.name == "energy") {
-        upLvl.addEventListener('click', e => {
-          const currItemCost = JSON.parse(localStorage.getItem('market-items'))[idx].cost
-          if(currItemCost < getScore()){
-
-            setEnergyLimit(getEnergyLimit() * 1.4)
-            setScore(getScore() - currItemCost)
-
-            infoItem.lvl += 1
-            infoItem.cost = +item.querySelector('.market-item__cost span').textContent * 2
-  
-  
-            const newInfo = {
-              name: marketItems[idx].name,
-              cost: infoItem.cost,
-              isActive: marketItems[idx].isActive,
-              lvl: infoItem.lvl + 1,
-            }
-            replaceItem(newInfo, idx)
-  
-            item.querySelector('.market-item__lvl span').textContent = infoItem.lvl
-            item.querySelector('.market-item__cost span').textContent = item.querySelector('.market-item__cost span').textContent * 2
-          }
-          
-        })
-      }
+      
     }
+  }
 
-    
-  });
+
+  function energyFunc(item){
+    if(item.cost < getScore()){
+
+      setEnergyLimit(getEnergyLimit() * 1.4)
+      setScore(getScore() - item.cost)
+
+      item.lvl += 1
+      item.cost *= 2
+
+    }
+  }
+
+  marketList.addEventListener('click', e => {
+    const parent = e.target.closest('.market-item');
+    if(parent){
+      const name = parent.querySelector('.market-item__name').textContent.trim().toLowerCase()
+      const id = parent.dataset.id
+      
+      let position = -1;
+      let item = marketItemsArr.find((item, i) => {
+        if(item.id == id){
+          position = i
+          return true
+        }
+      })
+      
+      if(name == "boost") {
+        boostFunc(item, position)
+      }
+      
+      if(name == "energy") {
+        energyFunc(item, position)
+      }
+
+      if(position != -1){
+        replaceItem(item, position)
+      }
+      position = -1
+      initItems()
+    }
+  })
+
+  
+
 
   function replaceItem(item, position){
-    marketItems.slice(position, 1, item)
+    marketItemsArr.slice(position, 1, item)
     localStorage.setItem('market-items', JSON.stringify(marketItemsArr))
   }
 
@@ -217,11 +218,11 @@ if (marketItems.length > 0) {
     marketItemsArr.push(item)
     localStorage.setItem('market-items', JSON.stringify(marketItemsArr))
   }
-  
+  localStorage.setItem('market-items', JSON.stringify(marketItemsArr))
   function getMarketCosts(){
     return JSON.parse(localStorage.getItem('market-items')) ?? marketItemsArr;
   }
-  
+
   function checkCosts(){
     const items = getMarketCosts()
     items.forEach(el => {
@@ -231,9 +232,39 @@ if (marketItems.length > 0) {
         el.isActive = true
       }
     })
-  
+
     localStorage.setItem('market-items', JSON.stringify(items))
   }
+
+  function initItems(){
+    const items = JSON.parse(localStorage.getItem('market-items'))
+    
+
+    const html = items.map(item => {
+      return `
+        <li class="market__item market-item" data-id="${item.id}">
+            <div class="market-item__top">
+              <div class="market-item__icon">
+                <img src="./assets/lvlmarket.jpg" alt="" />
+              </div>
+              <div class="market-item__about">
+                <div class="market-item__name">${item.name}</div>
+                <div class="market-item__desc">${item.desc}</div>
+              </div>
+            </div>
+            <div class="market-item__bottom">
+              <div class="market-item__lvl">lvl <span>${item.lvl}</span></div>
+              <div class="market-item__cost">
+                <img src="./assets/coin.png" alt="" />
+                <span>${item.cost}</span>
+              </div>
+            </div>
+          </li>
+      `
+    }).join('\n')
+    marketList.innerHTML = html
+  }
+initItems()
 }
 function addBoutghtClass(item){
   item.classList.add('market__item--bought')
